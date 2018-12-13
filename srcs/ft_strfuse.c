ecb88ea1e9287c7439ab6ebee0704d7f4e0fe696
Your Name <bleplat@student.42.fr>
2018-12-13 17:08:46 +0100

    updated strfuse

--- srcs/ft_strfuse.c
+++ srcs/ft_strfuse.c
@@ -6,7 +6,7 @@
 /*   By: bleplat <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:51:21 by bleplat           #+#    #+#             */
-/*   Updated: 2018/12/04 20:08:06 by bleplat          ###   ########.fr       */
+/*   Updated: 2018/12/09 13:44:45 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@
 
 /*
 ** Fuse *s1 and *s2, returning a pointer to a new string.
-** Free parameters depending on LIBFT_FREETASK, except
+** Free parameters depending on free_task value (see FT_FREETASK_*), except
 ** when one is NULL, because the other is returned.
 ** Otherwise *s1 and *s2 may be set to NULL.
 ** Return NULL on error or if both strings where NULL.
+** Do not perform free on error if free_task is not FT_FREETASK_ALWAY.
 */
 
 char		*ft_strfuse(char **s1, char **s2, int free_task)
@@ -31,15 +32,18 @@ char		*ft_strfuse(char **s1, char **s2, int free_task)
 	if (*s2 == NULL)
 		return (*s1);
 	new_str = ft_strjoin(*s1, *s2);
-	if (free_task & LIBFT_FREETASK_1ST)
+	if (new_str != NULL || (free_task & FT_FREETASK_ALWAY))
 	{
-		free(*s1);
-		*s1 = NULL;
-	}
-	if (free_task & LIBFT_FREETASK_2ND)
-	{
-		free(*s2);
-		*s2 = NULL;
+		if (free_task & FT_FREETASK_1ST)
+		{
+			free(*s1);
+			*s1 = NULL;
+		}
+		if (free_task & FT_FREETASK_2ND)
+		{
+			free(*s2);
+			*s2 = NULL;
+		}
 	}
 	return (new_str);
 }
